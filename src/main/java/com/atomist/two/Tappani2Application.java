41bf343ba3a3ce34ba676ef5ff0a931b83ba928c
Rod Johnson <johnsonroda@gmail.com>
2018-02-19 19:33:04 +1100

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/two/Tappani2Application.java
@@ -0,0 +1,12 @@
+package com.atomist.two;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tappani2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tappani2Application.class, args);
+	}
+}
